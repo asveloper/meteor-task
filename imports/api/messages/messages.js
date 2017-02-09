400850d8d594fd3474b6cde67fe4f1ad1050f6a3
Arslan Nasir <arslan@mobylogix.com>
2017-02-09 20:41:41 +0500

    Messages schema updated

--- imports/api/messages/messages.js
+++ imports/api/messages/messages.js
@@ -1,5 +1,16 @@
 // Definition of the messages collection
 
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
 export const Messages = new Mongo.Collection('messages');
+
+Messages.attachBehaviour('timestampable');
+
+Messages.allow({
+  insert: function(userId) {
+    if (Meteor.userId())
+      return true;
+    return false;
+  }
+});
