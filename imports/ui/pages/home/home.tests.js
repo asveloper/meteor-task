5f2f249c3d0090d8aafba6a5155f96a15fb4be8d
Arslan Nasir <arslan@mobylogix.com>
2017-02-13 20:31:40 +0500

    Client tests updated

--- imports/ui/pages/home/home.tests.js
+++ imports/ui/pages/home/home.tests.js
@@ -6,10 +6,6 @@ import { Messages } from '../../../api/messages/messages';
 if (Meteor.isClient) {
   describe('messages', function () {
     it('add new message by user', function () {
-      beforeEach(function () {
-        Messages.remove({});
-      });
-
       Messages.insert({
         description: "this is a test message",
         createdBy: Random.id()
